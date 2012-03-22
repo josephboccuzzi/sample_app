d811f1d1ecfe0fa5fea787083d0d3e4aefa24ac8
josephboccuzzi <joseph@designerpages.com>
2012-03-22 17:59:09 -0400

    Finished Chapter 5 exercises

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
   
+  def logo
+    logo = image_tag("logo.png", :alt => "Smple App", :class => "round" )
+  end
+  
+     
   # Return a title on a per-page basis.
   def title
     base_title = "Ruby on Rails Tutorial Sample App"
@@ -8,5 +13,5 @@ module ApplicationHelper
     else
       "#{base_title} | #{@title}"
     end
-  end
+  end 
 end
